bc6aee813d33eda1e424220bc6743fb1682cc3bc
Guilherme F Maurila <gfmaurila@gmail.com>
2021-12-27 18:55:44 -0300

    1 - C# .Net Core e RabbitMQ Queues Produtor e Consumidor

---
+++ 1 - C# .Net Core e RabbitMQ Queues Produtor e Consumidor/RProdutor/RProdutor/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RProdutor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "qWebApp",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                int count = 0;
+                while (true)
+                {
+                    string message = $"Hello World - {count++}";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "qWebApp",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                    System.Threading.Thread.Sleep(200);
+                }
+
+                
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # RabbitMQ
-RabbitMQ
+
+# Rodando RabbitMQ em local
+
+- $ docker run -d --hostname localhost --name local-rabbit -p 15672:15672 -p 5672:5672 rabbitmq:3-management
+- http://localhost:15672
+- usuário guest 
+- senha guest
+
+
