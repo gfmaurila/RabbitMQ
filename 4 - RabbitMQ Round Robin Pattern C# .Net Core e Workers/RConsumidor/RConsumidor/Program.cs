0ebefa86001b4e4c23cdd922e7b5276c4c09db68
Guilherme F Maurila <gfmaurila@gmail.com>
2021-12-28 11:44:33 -0300

    4 - RabbitMQ Round Robin Pattern C# .Net Core e Workers

---
+++ 4 - RabbitMQ Round Robin Pattern C# .Net Core e Workers/RConsumidor/RConsumidor/Program.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RConsumidor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "order",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (model, ea) =>
+                {
+                    try
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}", message);
+                        channel.BasicAck(ea.DeliveryTag, false);
+                    } catch (Exception ex)
+                    {
+                        // Log ex
+                        channel.BasicNack(ea.DeliveryTag, false, false);
+                    }
+                };
+
+                channel.BasicConsume(queue: "order",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
