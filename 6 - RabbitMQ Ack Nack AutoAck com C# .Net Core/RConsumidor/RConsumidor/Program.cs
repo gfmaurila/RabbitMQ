c8e7c425dbd35ebbf1875d2d95a89288dcd743cc
Guilherme F Maurila <gfmaurila@gmail.com>
2021-12-28 23:43:43 -0300

    6 - RabbitMQ Ack Nack AutoAck com C# .Net Core

---
+++ 6 - RabbitMQ Ack Nack AutoAck com C# .Net Core/RConsumidor/RConsumidor/Program.cs
@@ -0,0 +1,67 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RConsumidor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                for (int i1 = 0; i1 < 4; i1++)
+                {
+                    var channel = CreateChannel(connection);
+
+                    channel.QueueDeclare(queue: "order",
+                                    durable: false,
+                                    exclusive: false,
+                                    autoDelete: false,
+                                    arguments: null);
+
+                    for (int i2 = 0; i2 < 3; i2++)
+                    {
+                        BuildAndRunWorker(channel, $"Worker - {i1}:{i2}");
+                    }
+                }
+
+                Console.ReadLine();
+            }
+        }
+
+        public static IModel CreateChannel(IConnection connection)
+        {
+            var channel = connection.CreateModel();
+            return channel;
+        }
+
+        public static void BuildAndRunWorker(IModel channel, string workerName)
+        {
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                try
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($"channel: {channel.ChannelNumber} - workerName: {workerName} - [x] Received message: {message}");
+                    channel.BasicAck(ea.DeliveryTag, false);
+                }
+                catch (Exception ex)
+                {
+                    // Log ex
+                    Console.WriteLine(ex.Message);
+                    channel.BasicNack(ea.DeliveryTag, false, true);
+                }
+            };
+            channel.BasicConsume(queue: "order", autoAck: false, consumer: consumer);
+
+            //Console.WriteLine(" Press [enter] to exit.");
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ 6 - RabbitMQ Ack Nack AutoAck com C# .Net Core/RProdutor/RProdutor/Program.cs
@@ -0,0 +1,79 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RProdutor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() {
+                HostName = "localhost"
+            };
+            var manualResetEvent = new ManualResetEvent(false);
+
+            manualResetEvent.Reset();
+            using (var connection = factory.CreateConnection())
+            {
+                var queueName = "order";
+                var channel1 = CreateChannel(connection);
+
+                BuildPublishers(channel1, queueName, "Produtor A", manualResetEvent);
+
+                manualResetEvent.WaitOne();
+                Console.ReadLine();
+            }
+        }
+
+        public static IModel CreateChannel(IConnection connection)
+        {
+            var channel = connection.CreateModel();
+            return channel;
+        }
+
+        public static void BuildPublishers(IModel channel, string queue, string publisherName, ManualResetEvent manualResetEvent)
+        {
+            Task.Run(() => {
+
+                channel.QueueDeclare(queue: queue,
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+
+                int count = 0;
+                while (true)
+                {
+                    try
+                    {
+                        Console.WriteLine($"Pressione qualquer tecla para produzi 100 msg");
+                        Console.ReadLine();
+
+                        for (int i = 0; i < 100; i++)
+                        {
+                            string message = $"OrderNumber - {count++} from {publisherName}";
+
+                            var body = Encoding.UTF8.GetBytes(message);
+
+                            channel.BasicPublish("", queue, null, body);
+
+                            Console.WriteLine($" {publisherName} - [x] Sent {count}", message);
+                            //System.Threading.Thread.Sleep(1000);
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        manualResetEvent.Set();
+                    }
+                }
+
+            });
+        }
+    }
+}
