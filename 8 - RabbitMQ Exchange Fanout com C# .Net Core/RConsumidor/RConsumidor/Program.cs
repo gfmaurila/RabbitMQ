41e10842365d05ddba82548c04c08342c1cf0528
Guilherme F Maurila <gfmaurila@gmail.com>
2021-12-29 13:31:09 -0300

    8 - RabbitMQ Exchange Fanout com C# .Net Core

---
+++ 8 - RabbitMQ Exchange Fanout com C# .Net Core/RConsumidor/RConsumidor/Program.cs
@@ -0,0 +1,66 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RConsumidor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                var channel = CreateChannel(connection);
+
+                var queueName = args[0];
+
+                channel.QueueDeclare(queue: queueName,
+                                durable: false,
+                                exclusive: false,
+                                autoDelete: false,
+                                arguments: null);
+
+                BuildAndRunWorker(channel, $"Worker - A", queueName);
+                BuildAndRunWorker(channel, $"Worker - B", queueName);
+
+                Console.ReadLine();
+            }
+        }
+
+        public static IModel CreateChannel(IConnection connection)
+        {
+            var channel = connection.CreateModel();
+            return channel;
+        }
+
+        public static void BuildAndRunWorker(IModel channel, string workerName, string queueName)
+        {
+            channel.BasicQos(0, 7, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                try
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($"{channel.ChannelNumber} - {workerName} - [x] Received {message}");
+                    channel.BasicAck(ea.DeliveryTag, false);
+                }
+                catch (Exception ex)
+                {
+                    // Log ex
+                    Console.WriteLine(ex.Message);
+                    channel.BasicNack(ea.DeliveryTag, false, true);
+                }
+            };
+            channel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
+
+            //Console.WriteLine(" Press [enter] to exit.");
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ 8 - RabbitMQ Exchange Fanout com C# .Net Core/RProdutor/RProdutor/Program.cs
@@ -0,0 +1,82 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RProdutor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var manualResetEvent = new ManualResetEvent(false);
+
+            manualResetEvent.Reset();
+            using (var connection = factory.CreateConnection())
+            {
+                var queueName = "order";
+                var channel1 = SetupChannel(connection);
+                BuildPublishers(channel1, queueName, "Produtor A", manualResetEvent);
+
+                manualResetEvent.WaitOne();
+                Console.ReadLine();
+            }
+        }
+
+        public static IModel SetupChannel(IConnection connection)
+        {
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "order", durable: false, exclusive: false, autoDelete: false, arguments: null);
+            channel.QueueDeclare(queue: "logs", durable: false, exclusive: false, autoDelete: false, arguments: null);
+            channel.QueueDeclare(queue: "finance_orders", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            channel.ExchangeDeclare("order", "fanout");
+
+            channel.QueueBind("order", "order", "");
+            channel.QueueBind("logs", "order", "");
+            channel.QueueBind("finance_orders", "order", "");
+
+            return channel;
+        }
+
+        public static void BuildPublishers(IModel channel, string queue, string publisherName, ManualResetEvent manualResetEvent)
+        {
+            Task.Run(() => {
+
+                channel.QueueDeclare(queue: queue, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                int count = 0;
+                while (true)
+                {
+                    try
+                    {
+                        Console.WriteLine($"Pressione qualquer tecla para produzi 10 msg");
+                        Console.ReadLine();
+
+                        for (int i = 0; i < 10; i++)
+                        {
+                            string message = $"OrderNumber - {count++} from {publisherName}";
+
+                            var body = Encoding.UTF8.GetBytes(message);
+
+                            channel.BasicPublish("order", "", null, body);
+
+                            Console.WriteLine($" {publisherName} - [x] Sent {count}", message);
+                            //System.Threading.Thread.Sleep(1000);
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        manualResetEvent.Set();
+                    }
+                }
+
+            });
+        }
+    }
+}
