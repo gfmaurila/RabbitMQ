bd5c41250908fff27d8bba0a61172b5ea56678fa
Guilherme F Maurila <gfmaurila@gmail.com>
2021-12-30 01:50:45 -0300

    10 - RabbitMQ Exchange Topic

---
+++ 10 - RabbitMQ Exchange Topic/RConsumidor/RConsumidor/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RConsumidor
+{
+    public class Order
+    {
+        public long Id { get; private set; }
+        public DateTime CreateDate { get; }
+        public DateTime LastUpdated { get; private set; }
+        public long Amount { get; private set; }
+        public Order(long id, long amount)
+        {
+            Id = id;
+            Amount = amount;
+            CreateDate = LastUpdated = DateTime.UtcNow;
+        }
+
+        public void UpdateOrder(long amount)
+        {
+            Amount = amount;
+            LastUpdated = DateTime.UtcNow;
+        }
+    }
+}
