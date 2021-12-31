65e635b57da25b0dffb708f19a5d5377a9b57530
Guilherme F Maurila <gfmaurila@gmail.com>
2021-12-31 00:28:16 -0300

    13 - RabbitMQ Queue e Message TTL Expiration

---
+++ 13 - RabbitMQ Queue e Message TTL Expiration/RProdutor/RProdutor/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RProdutor
+{
+    class Program
+    {
+        static void Main()
+        {
+            var queueName = "test_time_to_live";
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    var arguments = new Dictionary<string, object>();
+                    arguments.Add("x-message-ttl", 22000);
+
+                    channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: arguments);
+
+                    var body = Encoding.UTF8.GetBytes($"Hello World! Data/Hora: {DateTime.Now}");
+
+
+                    channel.BasicPublish(exchange: "", routingKey: queueName, basicProperties: null, body: body);
+
+
+                    //var props = channel.CreateBasicProperties();
+                    //props.Expiration = "20000";
+                    //channel.BasicPublish(exchange: "", routingKey: queueName, basicProperties: props, body: body);
+                }
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
