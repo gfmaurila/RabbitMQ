255a07bf9302e4310bdf5ad1956e7dfc7668deed
Guilherme F Maurila <gfmaurila@gmail.com>
2021-12-31 00:58:08 -0300

    14 - RabbitMQ Persistence Message e Durable Queue

---
+++ 14 - RabbitMQ Persistence Message e Durable Queue/RProdutor/RProdutor/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RProdutor
+{
+    class Program
+    {
+        static void Main()
+        {
+            var queueName = "RabbitMQ_Persistence_Message_e_Durable_Queue";
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+                    var body = Encoding.UTF8.GetBytes($"Hello World! Data/Hora: {DateTime.Now}");
+
+                    var basicProperties = channel.CreateBasicProperties();
+                    basicProperties.Persistent = true;
+
+                    channel.BasicPublish(exchange: "", routingKey: queueName, basicProperties: basicProperties, body: body);
+                }
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
