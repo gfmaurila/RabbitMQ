52dcf84968293765120e61e1f6080fd1f0ed2270
Guilherme F Maurila <gfmaurila@gmail.com>
2022-01-02 13:09:34 -0300

    18 - MassTransit Worker API First com C# .Net e RabbitMQ

---
+++ 18 - MassTransit Worker API First com C# .Net e RabbitMQ/MassTransitConsumerAPIFirst/Consumer/OrderConsumer.cs
@@ -0,0 +1,37 @@
+﻿using MassTransit;
+using masstransit_consumer_api_first.Domain;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace masstransit_consumer_api_first.Consumer
+{
+    public sealed class OrderConsumer : IConsumer<Order>
+    {
+        private readonly ILogger<OrderConsumer> _logger;
+
+        public OrderConsumer(ILogger<OrderConsumer> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Consume(ConsumeContext<Order> context)
+        {
+            try
+            {
+                var order = context.Message;
+
+                _logger.LogInformation($"Order received: {order.OrderId}");
+
+            }catch(Exception ex)
+            {
+                _logger.LogError("Error on try to consume order", ex);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ 18 - MassTransit Worker API First com C# .Net e RabbitMQ/MassTransitConsumerAPIFirst/Domain/Order.cs
@@ -0,0 +1,7 @@
+﻿namespace masstransit_consumer_api_first.Domain
+{
+    public class Order
+    {
+        public long OrderId { get; set; }
+    }
+}
